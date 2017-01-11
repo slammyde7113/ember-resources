5e147c48b793379bed74aacec18b63e1094171ca
payne-chris-r <06cpayne@gmail.com>
2017-01-11 09:04:30 -0500

    Add button to 'edit' a title
    Button isn't hooked up to a route action yet, but does fire the
    associated action in the component file. Commiting so I can branch
    and come back.

--- app/components/listr-list/card/component.js
+++ app/components/listr-list/card/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    editList(){
+      console.log("You're inside editList in the listr-list/card component");
+    },
+  },
 });
