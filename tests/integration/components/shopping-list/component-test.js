8d63dbb75aee86421e3c624e4a767825b64db4ca
payne-chris-r <06cpayne@gmail.com>
2017-03-14 15:29:40 -0400

    Rename listr-list to shopping-list
    Renamed all component files and references to listr-list. In addition
    I've updated the tests.

--- tests/integration/components/shopping-list/component-test.js
+++ tests/integration/components/shopping-list/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('listr-list', 'Integration | Component | listr list', {
+moduleForComponent('shopping-list', 'Integration | Component | listr list', {
   integration: true,
 });
 
@@ -9,15 +9,15 @@ test('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{listr-list}}`);
+  this.render(hbs`{{shopping-list}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#listr-list}}
+    {{#shopping-list}}
       template block text
-    {{/listr-list}}
+    {{/shopping-list}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
--- tests/integration/components/shopping-list/item/component-test.js
+++ tests/integration/components/shopping-list/item/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('listr-list/item', 'Integration | Component | listr list/item', {
+moduleForComponent('shopping-list/item', 'Integration | Component | listr list/item', {
   integration: true,
 });
 
@@ -9,15 +9,15 @@ test('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{listr-list/item}}`);
+  this.render(hbs`{{shopping-list/item}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#listr-list/item}}
+    {{#shopping-list/item}}
       template block text
-    {{/listr-list/item}}
+    {{/shopping-list/item}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
