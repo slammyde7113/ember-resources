d54b0507ae04e56bbe2e06982048c1873f1dada5
racheltstevens <racheltstevens@gmail.com>
2017-03-22 12:21:14 -0400

    Persist changes in item to the database
    Change listItemCompleted to be an alias for item.done
    Refactor the toggleDone action to send the action up with the item
    Send the toggleDone action up to the route (as toggleItemDone)
    Persist changes to the API in the route toggleItemDone action

--- app/components/shopping-list.js
+++ app/components/shopping-list.js
@@ -8,5 +8,9 @@ export default Ember.Component.extend({
     toggleListDetail () {
       return this.toggleProperty('listDetailHidden');
     },
+
+    toggleItemDone (item) {
+      return this.sendAction('toggleItemDone', item);
+    },
   },
 });
