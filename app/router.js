964f2b14029cecc4bf8dafc0940691825d9b102b
racheltstevens <racheltstevens@gmail.com>
2017-03-22 11:30:24 -0400

    Create list route
    Create item model
    Add relationships between list and item
    Use shopping-list component to display a list
    Use list route to get list data from the api using params

--- app/router.js
+++ app/router.js
@@ -3,11 +3,12 @@ import config from './config/environment';
 
 const Router = Ember.Router.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
 });
 
-Router.map(function() {
+Router.map(function () {
   this.route('lists');
+  this.route('list', { path: '/lists/:list_id' });
 });
 
 export default Router;
